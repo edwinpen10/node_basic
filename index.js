0991e7dddd2781798c0e2a20188eec5d1f751fe9
Webslesson <edwinpen010@gmail.com>
2019-11-10 21:32:33 -0500

    update

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 
+// for parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    var x, y, z
+    x = 5
+    y = 3
+    z = x + y
+
+    return res.send(`The value of z is ${z}`)
+})
+
+
+
 app.listen(3300, () => {
     console.log(`Example app listening on port 3300`)
 })
